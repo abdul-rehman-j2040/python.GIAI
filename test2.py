32522587a4e9703c17a9ebd1a80b7d1abf06526d
abdul-rehman-j2040 <a.rehman.j130@gmail.com>
2025-03-04 01:20:03 +0500

    new updates

---
+++ test2.py
@@ -0,0 +1,12 @@
+# num = int(input("\t\t\t Enter the number: ")) 
+
+# factorial = 1
+# for i in range(num, 0, -1):
+#     factorial *= i  
+
+# print(f"The factorial of the given number {num} is {factorial}")
+
+num = int(input(f"\t\t\t Enter the number :")) 
+i = 0
+for i in range(num,0,-1):
+    print(f"The factorianl of the given number is {num*i}")
